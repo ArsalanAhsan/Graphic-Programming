e6243ef0320a9cd4a2989a1738db63a0451fa35e
DESKTOP-4TV85P3\ZA bawani <ahsanarsalan41@gmail.com>
2024-11-14 18:42:30 +0500

    implement static Pong game UI with JOGL (no gameplay)

--- Assignment-02/src/FirstTriangle.java
+++ Assignment-02/src/FirstTriangle.java
@@ -1,165 +1,165 @@
-import java.awt.*;
-import javax.swing.*;
-import com.jogamp.opengl.*;
-import com.jogamp.opengl.awt.GLCanvas;
-import com.jogamp.opengl.awt.GLJPanel;
-import com.jogamp.opengl.util.FPSAnimator;
-import static com.jogamp.opengl.GL.*;
-import static com.jogamp.opengl.GL2.*;
-
-class Renderer {
-
-    // Array of 144 float values representing 72 2D points
-    float[] array = {
-            0.29761907f, 0.45421583f,
-            0.20238096f, 0.29368397f,
-            0.74999997f, 0.29368397f,
-            0.65476197f, 0.45421583f,
-            -0.02701211f, 0.7499916f,
-            -0.02701211f, -0.75000379f,
-            0.10714287f, -0.74977316f,
-            0.10714287f, 0.74852419f,
-            0.34523807f, 0.29368397f,
-            0.34523807f, -0.1647749f,
-            0.47714287f, -0.1647749f,
-            0.47714287f, 0.29368397f,
-            0.47714287f, 0.29368397f,
-            0.47714287f, -0.1647749f,
-            0.60714287f, -0.1647749f,
-            0.60714287f, 0.29368397f,
-            0.65476197f, 0.29368397f,
-            0.65476197f, -0.10764567f,
-            0.74999997f, -0.10764567f,
-            0.74999997f, 0.29368397f,
-            0.20238096f, 0.29368397f,
-            0.20238096f, -0.10764567f,
-            0.29761907f, -0.10764567f,
-            0.29761907f, 0.29368397f,
-            0.41666667f, 0.74852424f,
-            0.36904767f, 0.641503f,
-            0.58333337f, 0.641503f,
-            0.53571427f, 0.74852424f,
-            0.41666667f, 0.53448177f,
-            0.36904767f, 0.64150297f,
-            0.58333337f, 0.64150297f,
-            0.53571427f, 0.53448177f,
-            -0.60714285f, 0.45421583f,
-            -0.70238095f, 0.29368397f,
-            -0.1547619f, 0.29368397f,
-            -0.24999999f, 0.45421583f,
-            -0.5595238f, -0.29493284f,
-            -0.5595238f, -0.7497731f,
-            -0.46428571f, -0.7497731f,
-            -0.46428571f, -0.29493284f,
-            -0.39285714f, -0.29493284f,
-            -0.39285714f, -0.7497731f,
-            -0.29761904f, -0.7497731f,
-            -0.29761904f, -0.29493284f,
-            -0.24999999f, 0.29368397f,
-            -0.20238094f, -0.05413505f,
-            -0.10714285f, -0.02737975f,
-            -0.1547619f, 0.29368397f,
-            -0.48809523f, 0.74852424f,
-            -0.53571428f, 0.641503f,
-            -0.32142856f, 0.641503f,
-            -0.36904761f, 0.74852424f,
-            -0.48809523f, 0.53448177f,
-            -0.53571428f, 0.64150297f,
-            -0.32142856f, 0.64150297f,
-            -0.36904761f, 0.53448177f,
-            -0.5595238f, 0.29368397f,
-            -0.67857142f, -0.29493284f,
-            -0.17857142f, -0.29493284f,
-            -0.32142856f, 0.29368397f,
-            -0.60714285f, 0.29368397f,
-            -0.6547619f, -0.05413505f,
-            -0.75f, -0.02737975f,
-            -0.70238095f, 0.29368397f,
-            0.51190477f, -0.1647749f,
-            0.51190477f, -0.74977308f,
-            0.60714287f, -0.74977308f,
-            0.60714287f, -0.1647749f,
-            0.34523807f, -0.1647749f,
-            0.34523807f, -0.74977308f,
-            0.44047617f, -0.74977308f,
-            0.44047617f, -0.1647749f
-    };
-
-    public void init(GLAutoDrawable d) {}
-
-    public void resize(GLAutoDrawable d, int width, int height) {
-        GL2 gl = d.getGL().getGL2(); // get the OpenGL 2 graphics context
-        gl.glViewport(0, 0, width, height);
-    }
-
-    public void display(GLAutoDrawable d) {
-        GL2 gl = d.getGL().getGL2();  // get the OpenGL 2 graphics context
-        gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-        gl.glClear(GL.GL_COLOR_BUFFER_BIT);
-        gl.glLoadIdentity();
-        gl.glOrtho(-1.0f, 1.0f, -1.0f, 1.0f, -1.0f, 1.0f);
-        gl.glColor3f(1.0f, 1.0f, 1.0f);
-
-        // Draw points
-        gl.glBegin(GL_POINTS);
-        for (int i = 0; i < array.length; i += 2) {
-            gl.glVertex2f(array[i], array[i + 1]); // use only x, y coordinates
-        }
-        gl.glEnd();
-    }
-
-    public void dispose(GLAutoDrawable d) {}
-}
-
-class MyGui extends JFrame implements GLEventListener {
-
-    private Renderer renderer;
-
-    public void createGUI() {
-        setTitle("Jogl Points Visualization");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        GLProfile glp = GLProfile.get(GLProfile.GL2);
-        GLCapabilities caps = new GLCapabilities(glp);
-        GLJPanel canvas = new GLJPanel(caps); // Use GLJPanel instead of GLCanvas
-        setSize(800, 800);
-        getContentPane().add(canvas);
-        final FPSAnimator ani = new FPSAnimator(canvas, 60, true);
-        canvas.addGLEventListener(this);
-        setVisible(true);
-        renderer = new Renderer();
-        ani.start();
-    }
-
-    @Override
-    public void init(GLAutoDrawable d) {
-        renderer.init(d);
-    }
-
-    @Override
-    public void reshape(GLAutoDrawable d, int x, int y, int width, int height) {
-        renderer.resize(d, width, height);
-    }
-
-    @Override
-    public void display(GLAutoDrawable d) {
-        renderer.display(d);
-    }
-
-    @Override
-    public void dispose(GLAutoDrawable d) {
-        renderer.dispose(d);
-    }
-}
-
-public class FirstTriangle {
-    public static void main(String[] args) {
-        System.setProperty("sun.java2d.uiScale", "1.0");
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                MyGui myGUI = new MyGui();
-                myGUI.createGUI();
-            }
-        });
-    }
-}
+//import java.awt.*;
+//import javax.swing.*;
+//import com.jogamp.opengl.*;
+//import com.jogamp.opengl.awt.GLCanvas;
+//import com.jogamp.opengl.awt.GLJPanel;
+//import com.jogamp.opengl.util.FPSAnimator;
+//import static com.jogamp.opengl.GL.*;
+//import static com.jogamp.opengl.GL2.*;
+//
+//class Renderer {
+//
+//    // Array of 144 float values representing 72 2D points
+//    float[] array = {
+//            0.29761907f, 0.45421583f,
+//            0.20238096f, 0.29368397f,
+//            0.74999997f, 0.29368397f,
+//            0.65476197f, 0.45421583f,
+//            -0.02701211f, 0.7499916f,
+//            -0.02701211f, -0.75000379f,
+//            0.10714287f, -0.74977316f,
+//            0.10714287f, 0.74852419f,
+//            0.34523807f, 0.29368397f,
+//            0.34523807f, -0.1647749f,
+//            0.47714287f, -0.1647749f,
+//            0.47714287f, 0.29368397f,
+//            0.47714287f, 0.29368397f,
+//            0.47714287f, -0.1647749f,
+//            0.60714287f, -0.1647749f,
+//            0.60714287f, 0.29368397f,
+//            0.65476197f, 0.29368397f,
+//            0.65476197f, -0.10764567f,
+//            0.74999997f, -0.10764567f,
+//            0.74999997f, 0.29368397f,
+//            0.20238096f, 0.29368397f,
+//            0.20238096f, -0.10764567f,
+//            0.29761907f, -0.10764567f,
+//            0.29761907f, 0.29368397f,
+//            0.41666667f, 0.74852424f,
+//            0.36904767f, 0.641503f,
+//            0.58333337f, 0.641503f,
+//            0.53571427f, 0.74852424f,
+//            0.41666667f, 0.53448177f,
+//            0.36904767f, 0.64150297f,
+//            0.58333337f, 0.64150297f,
+//            0.53571427f, 0.53448177f,
+//            -0.60714285f, 0.45421583f,
+//            -0.70238095f, 0.29368397f,
+//            -0.1547619f, 0.29368397f,
+//            -0.24999999f, 0.45421583f,
+//            -0.5595238f, -0.29493284f,
+//            -0.5595238f, -0.7497731f,
+//            -0.46428571f, -0.7497731f,
+//            -0.46428571f, -0.29493284f,
+//            -0.39285714f, -0.29493284f,
+//            -0.39285714f, -0.7497731f,
+//            -0.29761904f, -0.7497731f,
+//            -0.29761904f, -0.29493284f,
+//            -0.24999999f, 0.29368397f,
+//            -0.20238094f, -0.05413505f,
+//            -0.10714285f, -0.02737975f,
+//            -0.1547619f, 0.29368397f,
+//            -0.48809523f, 0.74852424f,
+//            -0.53571428f, 0.641503f,
+//            -0.32142856f, 0.641503f,
+//            -0.36904761f, 0.74852424f,
+//            -0.48809523f, 0.53448177f,
+//            -0.53571428f, 0.64150297f,
+//            -0.32142856f, 0.64150297f,
+//            -0.36904761f, 0.53448177f,
+//            -0.5595238f, 0.29368397f,
+//            -0.67857142f, -0.29493284f,
+//            -0.17857142f, -0.29493284f,
+//            -0.32142856f, 0.29368397f,
+//            -0.60714285f, 0.29368397f,
+//            -0.6547619f, -0.05413505f,
+//            -0.75f, -0.02737975f,
+//            -0.70238095f, 0.29368397f,
+//            0.51190477f, -0.1647749f,
+//            0.51190477f, -0.74977308f,
+//            0.60714287f, -0.74977308f,
+//            0.60714287f, -0.1647749f,
+//            0.34523807f, -0.1647749f,
+//            0.34523807f, -0.74977308f,
+//            0.44047617f, -0.74977308f,
+//            0.44047617f, -0.1647749f
+//    };
+//
+//    public void init(GLAutoDrawable d) {}
+//
+//    public void resize(GLAutoDrawable d, int width, int height) {
+//        GL2 gl = d.getGL().getGL2(); // get the OpenGL 2 graphics context
+//        gl.glViewport(0, 0, width, height);
+//    }
+//
+//    public void display(GLAutoDrawable d) {
+//        GL2 gl = d.getGL().getGL2();  // get the OpenGL 2 graphics context
+//        gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+//        gl.glClear(GL.GL_COLOR_BUFFER_BIT);
+//        gl.glLoadIdentity();
+//        gl.glOrtho(-1.0f, 1.0f, -1.0f, 1.0f, -1.0f, 1.0f);
+//        gl.glColor3f(1.0f, 1.0f, 1.0f);
+//
+//        // Draw points
+//        gl.glBegin(GL_POINTS);
+//        for (int i = 0; i < array.length; i += 2) {
+//            gl.glVertex2f(array[i], array[i + 1]); // use only x, y coordinates
+//        }
+//        gl.glEnd();
+//    }
+//
+//    public void dispose(GLAutoDrawable d) {}
+//}
+//
+//class MyGui extends JFrame implements GLEventListener {
+//
+//    private Renderer renderer;
+//
+//    public void createGUI() {
+//        setTitle("Jogl Points Visualization");
+//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//
+//        GLProfile glp = GLProfile.get(GLProfile.GL2);
+//        GLCapabilities caps = new GLCapabilities(glp);
+//        GLJPanel canvas = new GLJPanel(caps); // Use GLJPanel instead of GLCanvas
+//        setSize(800, 800);
+//        getContentPane().add(canvas);
+//        final FPSAnimator ani = new FPSAnimator(canvas, 60, true);
+//        canvas.addGLEventListener(this);
+//        setVisible(true);
+//        renderer = new Renderer();
+//        ani.start();
+//    }
+//
+//    @Override
+//    public void init(GLAutoDrawable d) {
+//        renderer.init(d);
+//    }
+//
+//    @Override
+//    public void reshape(GLAutoDrawable d, int x, int y, int width, int height) {
+//        renderer.resize(d, width, height);
+//    }
+//
+//    @Override
+//    public void display(GLAutoDrawable d) {
+//        renderer.display(d);
+//    }
+//
+//    @Override
+//    public void dispose(GLAutoDrawable d) {
+//        renderer.dispose(d);
+//    }
+//}
+//
+//public class FirstTriangle {
+//    public static void main(String[] args) {
+//        System.setProperty("sun.java2d.uiScale", "1.0");
+//        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+//            public void run() {
+//                MyGui myGUI = new MyGui();
+//                myGUI.createGUI();
+//            }
+//        });
+//    }
+//}
---
+++ Assignment-02/src/TriangleTransform.java
@@ -0,0 +1,137 @@
+import java.awt.*;
+import javax.swing.*;
+import com.jogamp.opengl.*;
+import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.awt.GLJPanel;
+import com.jogamp.opengl.util.FPSAnimator;
+import static com.jogamp.opengl.GL2.*;
+
+class Renderer {
+
+    float[] barData = { 0.02f, 0.2f, -0.02f, 0.2f, -0.02f, -0.2f, 0.02f, -0.2f };
+
+    float[] ballData = { 0.02f, 0.02f, -0.02f, 0.02f, -0.02f, -0.02f, 0.02f, -0.02f };
+
+    float[] score0Data = { 0.06f, 0.1f, 0.04f, 0.1f, 0.04f, -0.1f, 0.06f,
+            -0.1f, -0.04f, 0.1f, -0.06f, 0.1f, -0.06f, -0.1f, -0.04f, -0.1f,
+            0.05f, 0.1f, 0.05f, 0.08f, -0.05f, 0.08f, -0.05f, 0.1f, 0.05f,
+            -0.08f, 0.05f, -0.1f, -0.05f, -0.1f, -0.05f, -0.08f };
+
+    float[] score1Data = { 0.01f, 0.1f, -0.01f, 0.1f, -0.01f, -0.1f, 0.01f,
+            -0.1f };
+
+    float[] score2Data = { 0.06f, 0.1f, 0.04f, 0.1f, 0.04f, 0.0f,
+            0.06f, 0.0f, -0.04f, 0.0f, -0.06f, 0.0f,
+            -0.06f, -0.1f, -0.04f, -0.1f, 0.05f, 0.1f, 0.05f,
+            0.08f, -0.05f, 0.08f, -0.05f, 0.1f, 0.05f, -0.08f, 0.05f,
+            -0.1f, -0.05f, -0.1f, -0.05f, -0.08f, 0.05f,
+            0.01f, 0.05f, -0.01f, -0.05f, -0.01f, -0.05f,
+            0.01f };
+
+    float[] score3Data = { 0.06f, 0.1f, 0.04f, 0.1f, 0.04f, -0.1f, 0.06f,
+            -0.1f, 0.05f, 0.1f, 0.05f, 0.08f, -0.05f, 0.08f, -0.05f, 0.1f,
+            0.05f, -0.08f, 0.05f, -0.1f, -0.05f, -0.1f, -0.05f, -0.08f, 0.05f,
+            0.01f, 0.05f, -0.01f, -0.05f, -0.01f, -0.05f, 0.01f};
+    // Helper method to draw a quad based on vertex data
+    private void drawQuad(GL2 gl, float[] vertices) {
+        gl.glBegin(GL_QUADS);
+        for (int i = 0; i < vertices.length; i += 2) {
+            gl.glVertex2f(vertices[i], vertices[i + 1]);
+        }
+        gl.glEnd();
+    }
+
+    public void init(GLAutoDrawable d) {}
+
+    public void resize(GLAutoDrawable d, int width, int height) {
+        GL2 gl = d.getGL().getGL2();
+        gl.glViewport(0, 0, width, height);
+    }
+
+    public void display(GLAutoDrawable d) {
+        GL2 gl = d.getGL().getGL2();
+        gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        gl.glClear(GL_COLOR_BUFFER_BIT);
+        gl.glLoadIdentity();
+        gl.glOrtho(-1.0f, 1.0f, -1.0f, 1.0f, -1.0f, 1.0f);
+
+        gl.glColor3f(1.0f, 1.0f, 1.0f);
+
+        // Draw left paddle
+        gl.glPushMatrix();
+        gl.glTranslatef(-0.9f, 0.0f, 0.0f); // Position paddle on the left
+        drawQuad(gl, barData);
+        gl.glPopMatrix();
+
+        // Draw right paddle
+        gl.glPushMatrix();
+        gl.glTranslatef(0.9f, 0.0f, 0.0f); // Position paddle on the right
+        drawQuad(gl, barData);
+        gl.glPopMatrix();
+
+        // Draw ball in the center
+        gl.glPushMatrix();
+        gl.glTranslatef(0.0f, 0.0f, 0.0f);
+        drawQuad(gl, ballData);
+        gl.glPopMatrix();
+
+        // Draw score "1" at the top center as an example
+        gl.glPushMatrix();
+        gl.glTranslatef(0.0f, 0.8f, 0.0f);
+        drawQuad(gl, score1Data);
+        gl.glPopMatrix();
+    }
+
+    public void dispose(GLAutoDrawable d) {}
+}
+
+class MyGui extends JFrame implements GLEventListener {
+
+    private Renderer renderer;
+
+    public void createGUI() {
+        setTitle("Pong Game");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        GLProfile glp = GLProfile.getDefault();
+        GLCapabilities caps = new GLCapabilities(glp);
+        GLJPanel canvas = new GLJPanel(caps);
+        setSize(400, 400);
+        getContentPane().add(canvas);
+        final FPSAnimator ani = new FPSAnimator(canvas, 60, true);
+        canvas.addGLEventListener(this);
+        setVisible(true);
+        renderer = new Renderer();
+        ani.start();
+    }
+
+    @Override
+    public void init(GLAutoDrawable d) {
+        renderer.init(d);
+    }
+
+    @Override
+    public void reshape(GLAutoDrawable d, int x, int y, int width, int height) {
+        renderer.resize(d, width, height);
+    }
+
+    @Override
+    public void display(GLAutoDrawable d) {
+        renderer.display(d);
+    }
+
+    @Override
+    public void dispose(GLAutoDrawable d) {
+        renderer.dispose(d);
+    }
+}
+
+public class TriangleTransform {
+    public static void main(String[] args) {
+        System.setProperty("sun.java2d.uiScale", "1.0");
+        SwingUtilities.invokeLater(() -> {
+            MyGui myGUI = new MyGui();
+            myGUI.createGUI();
+        });
+    }
+}
